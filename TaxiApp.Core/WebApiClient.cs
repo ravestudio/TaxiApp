c55a9d57ac0000cbcb3e987def62e2e473995073
ravestudio <ravestudio@mail.ru>
2016-07-27 23:35:36 +0600

    webclient

--- TaxiApp.Core/WebApiClient.cs
+++ TaxiApp.Core/WebApiClient.cs
@@ -10,34 +10,26 @@ namespace TaxiApp.Core
 {
     public class WebApiClient
     {
-        public async Task<string> GetData(string url, List<KeyValuePair<string, string>> data)
+        public Task<string> GetData(string url, List<KeyValuePair<string, string>> data)
         {
-
-            string res = string.Empty;
-
-            //byte[] dataStream = Encoding.UTF8.GetBytes(data);
+            TaskCompletionSource<string> TCS = new TaskCompletionSource<string>();
 
             var uri = new Uri(url);
             System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
             System.Net.Http.HttpContent content = new System.Net.Http.FormUrlEncodedContent(data);
 
-            //httpClient.PostAsync(uri, content);
-
             string text = string.Empty;
 
-            try
-            {
-                System.Net.Http.HttpResponseMessage response = await httpClient.PostAsync(uri, content);
 
-                text = await response.Content.ReadAsStringAsync();
-            }
-            catch(Exception ex)
-            {
-                string msg = ex.Message;
-            }
+            Task<System.Net.Http.HttpResponseMessage> response = httpClient.PostAsync(uri, content);
 
-            return text;
+            response.ContinueWith(r =>
+            {
+                string msg = r.Result.Content.ReadAsStringAsync().Result;
+                TCS.SetResult(msg);
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            return TCS.Task;
         }
-
     }
 }
