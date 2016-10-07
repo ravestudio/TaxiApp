53645dff81ca6174868749e1d77f7f926defe973
ravestudio <ravestudio@mail.ru>
2016-10-07 14:29:08 +0300

    создание контрола

---
+++ TaxiApp.Core.UWP/Controls/ListPickerFlyout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace TaxiApp.Core.UWP.Controls
+{
+    public class ListPickerFlyout : Flyout
+    {
+        public ListPickerFlyout()
+        {
+            this.Content = new Grid()
+            {
+                Width = 100,
+                Height = 200
+            };
+        }
+    }
+}
